f5de959ca6c30edddedd63390c976ab270050323
Divine Obeten <divineobeten9@gmail.com>
2023-01-23 09:19:16 +0100

    added images

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from 'react';
+import Pic1 from '../assets/amira-el-fohail-HeNS1RuPZdo-unsplash.jpg';
+import Pic2 from "../assets/kit-suman-xvj4GLyGXwc-unsplash.jpg";
+import Pic3 from '../assets/melody-temple-QvEULJG78U8-unsplash.jpg';
+import Pic4 from '../assets/victor-he-bVXvntN75zY-unsplash.jpg';
+import Portrait from '../assets/andreas-dress-pPfri15Zg5I-unsplash.jpg';
 
 const Grid = () => {
   return (
-    <div>
-      
+    <div className='grid'>
+      <img src={Pic1} alt="" />
+      <img src={Pic2} alt="" />
+      <img src={Pic3} alt="" />
+      <img src={Pic4} alt="" />
+      <img src={Portrait} alt="" />
     </div>
   )
 }
